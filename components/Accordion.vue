b7683d27e5a04362a596780f45ab6fd5f30202f8
Oleksii Mantserov <a.v.mantserov@gmail.com>
2024-04-07 22:03:10 +0300

    test-commit

---
+++ components/Accordion.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { items } from '~/vars/items';
+</script>
+
+<template>
+  <div class="container">
+    <div class="header">
+      <Icon class="mr-2" name="mage:question-mark-circle" size="1.5em" />
+      <p>FAQs</p>
+    </div>
+    <UAccordion
+      class="accordion"
+      variant="solid"
+      size="xl"
+      color="white"
+      multiple
+      :items="items"
+    />
+  </div>
+</template>
+
+<style>
+.header {
+  color: #333;
+  display: flex;
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.container {
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
+  background-color: #fbfbfb;
+  padding: 30px 40px 30px 40px;
+  max-width: 95%;
+  margin: 25px auto;
+}
+.accordion {
+  margin-top: 20px;
+}
+</style>
---
+++ vars/items.js
@@ -0,0 +1,33 @@
+export const items = [
+  {
+    label: 'What is a Private Lender?',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum nihil voluptates dolorum sed aliquam maxime! Voluptate minima, minus a itaque, consequuntur, tempora consectetur voluptatum reprehenderit veritatis doloribus numquam praesentium nulla aliquid ipsam labore perspiciatis consequatur quia ut necessitatibus blanditiis exercitationem! Illo veniam quod accusantium ipsa delectus doloribus ipsam ab enim dolorem recusandae. Enim, deserunt dicta quasi vel dolore sapiente in, dolor corporis labore magni molestias quidem praesentium, quas et? Temporibus nobis debitis explicabo fugiat cum molestias eveniet officia animi amet similique rerum aliquam eius quas porro est laboriosam, quidem reprehenderit. Neque unde deserunt id in ducimus voluptatibus sint sequi.',
+    defaultOpen: true,
+  },
+  {
+    label: 'Who is lending me the money?',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum nihil voluptates dolorum sed aliquam maxime! Voluptate minima, minus a itaque, consequuntur, tempora consectetur voluptatum reprehenderit veritatis doloribus numquam praesentium nulla aliquid ipsam labore perspiciatis consequatur quia ut necessitatibus blanditiis exercitationem! Illo veniam quod accusantium ipsa delectus doloribus ipsam ab enim dolorem recusandae. Enim, deserunt dicta quasi vel dolore sapiente in, dolor corporis labore magni molestias quidem praesentium, quas et? Temporibus nobis debitis explicabo fugiat cum molestias eveniet officia animi amet similique rerum aliquam eius quas porro est laboriosam, quidem reprehenderit. Neque unde deserunt id in ducimus voluptatibus sint sequi.',
+  },
+  {
+    label: 'What if I do not have a property?',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum nihil voluptates dolorum sed aliquam maxime! Voluptate minima, minus a itaque, consequuntur, tempora consectetur voluptatum reprehenderit veritatis doloribus numquam praesentium nulla aliquid ipsam labore perspiciatis consequatur quia ut necessitatibus blanditiis exercitationem! Illo veniam quod accusantium ipsa delectus doloribus ipsam ab enim dolorem recusandae. Enim, deserunt dicta quasi vel dolore sapiente in, dolor corporis labore magni molestias quidem praesentium, quas et? Temporibus nobis debitis explicabo fugiat cum molestias eveniet officia animi amet similique rerum aliquam eius quas porro est laboriosam, quidem reprehenderit. Neque unde deserunt id in ducimus voluptatibus sint sequi.',
+  },
+  {
+    label: 'What if I do not have experience?',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum nihil voluptates dolorum sed aliquam maxime! Voluptate minima, minus a itaque, consequuntur, tempora consectetur voluptatum reprehenderit veritatis doloribus numquam praesentium nulla aliquid ipsam labore perspiciatis consequatur quia ut necessitatibus blanditiis exercitationem! Illo veniam quod accusantium ipsa delectus doloribus ipsam ab enim dolorem recusandae. Enim, deserunt dicta quasi vel dolore sapiente in, dolor corporis labore magni molestias quidem praesentium, quas et? Temporibus nobis debitis explicabo fugiat cum molestias eveniet officia animi amet similique rerum aliquam eius quas porro est laboriosam, quidem reprehenderit. Neque unde deserunt id in ducimus voluptatibus sint sequi.',
+  },
+  {
+    label: 'What paperwork will I need?',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum nihil voluptates dolorum sed aliquam maxime! Voluptate minima, minus a itaque, consequuntur, tempora consectetur voluptatum reprehenderit veritatis doloribus numquam praesentium nulla aliquid ipsam labore perspiciatis consequatur quia ut necessitatibus blanditiis exercitationem! Illo veniam quod accusantium ipsa delectus doloribus ipsam ab enim dolorem recusandae. Enim, deserunt dicta quasi vel dolore sapiente in, dolor corporis labore magni molestias quidem praesentium, quas et? Temporibus nobis debitis explicabo fugiat cum molestias eveniet officia animi amet similique rerum aliquam eius quas porro est laboriosam, quidem reprehenderit. Neque unde deserunt id in ducimus voluptatibus sint sequi.',
+  },
+  {
+    label: 'What type of fees are there?',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum nihil voluptates dolorum sed aliquam maxime! Voluptate minima, minus a itaque, consequuntur, tempora consectetur voluptatum reprehenderit veritatis doloribus numquam praesentium nulla aliquid ipsam labore perspiciatis consequatur quia ut necessitatibus blanditiis exercitationem! Illo veniam quod accusantium ipsa delectus doloribus ipsam ab enim dolorem recusandae. Enim, deserunt dicta quasi vel dolore sapiente in, dolor corporis labore magni molestias quidem praesentium, quas et? Temporibus nobis debitis explicabo fugiat cum molestias eveniet officia animi amet similique rerum aliquam eius quas porro est laboriosam, quidem reprehenderit. Neque unde deserunt id in ducimus voluptatibus sint sequi.',
+  },
+];
